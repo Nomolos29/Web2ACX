fded48bcac87481ed784733e910d348cc39386f8
Nomolos29 <nomolos2019@gmail.com>
2024-01-27 21:28:50 +0100

    UI completed and dark mode feature implemented

--- KiddieCash/index.js
+++ KiddieCash/index.js
@@ -1,1 +1,15 @@
-ECHO is on.
+const themeSwitcher = document.getElementById('dark');
+themeSwitcher.classList.add("uil-brightness");
+
+themeSwitcher.onclick = function() {
+    document.body.classList.toggle("dark-mode");
+    if(themeSwitcher.classList.contains("uil-moon")){
+        themeSwitcher.classList.remove("uil-moon")
+        
+        themeSwitcher.classList.toggle("uil-brightness");
+    }else{
+        
+        themeSwitcher.classList.remove("uil-brightness")
+        themeSwitcher.classList.toggle("uil-moon");
+    };
+}
