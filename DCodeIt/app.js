ac0ac564a660a8836df49a13b920f42e588713df
Nomolos29 <nomolos2019@gmail.com>
2024-02-01 16:30:04 +0100

    activated light and dark mode

--- DCodeIt/app.js
+++ DCodeIt/app.js
@@ -1,1 +1,18 @@
-ECHO is on.
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const dark_mode = document.querySelector("#dark");
+const body = document.querySelector("body");
+
+
+// if Change Theme button clicked
+dark_mode.onclick = ()=>{
+    body.classList.toggle("dark-mode");
+}
