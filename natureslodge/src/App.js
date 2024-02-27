c83c71a2af5c36ebae4d2fead04c96c7b97d876c
Nomolos29 <nomolos2019@gmail.com>
2024-02-27 11:33:04 +0100

    Added a new folder

--- natureslodge/src/App.js
+++ natureslodge/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Annouce() {
+  return (
+    <h1>Nomolos29 just hacked this, Yeh!!</h1>
+  );
+};
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Annouce />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +28,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
